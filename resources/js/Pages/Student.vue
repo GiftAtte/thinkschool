a5290d4c91bca31683c30c6b132c05966b1bf52d
GiftAtte <attegift@gmail.com>
2021-03-31 21:56:04 +0100

    initial commit

---
+++ resources/js/Pages/Student.vue
@@ -0,0 +1,36 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="h4 font-weight-bold">
+            Student
+            </h2>
+        </template>
+              <div class="card card-body">
+                  <table class="table">
+                  <tr v-for="user in users" :key="user.id">
+                         <td>{{user.id}}</td>
+                         <td>{{user.name}}</td>
+                         <td>{{user.email}}</td>
+                  </tr>
+                  </table>
+              </div>
+
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+
+    export default {
+        props:['users'],
+
+
+        components: {
+            AppLayout,
+
+        },
+        created(){
+            console.log(this.users)
+        }
+    }
+</script>
